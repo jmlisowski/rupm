73156d381a35f1443f0bfcbf35f4695168139554
Jake Lisowski <jake@pop-os.localdomain>
2023-02-09 21:53:28 -0600

    update files

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -5,7 +5,7 @@ use std::path::PathBuf;
 use std::io;
 use std::fs::{
     File,
-    create_dir,
+    create_dir_all, remove_file,
 };
 
 fn rupmdir() -> PathBuf {
@@ -20,40 +20,45 @@ fn download(link: &str, filename: &str) {
     let mut out = File::create(filename).expect("failed to create file");
     io::copy(&mut body.as_bytes(), &mut out).expect("failed to copy content");
 }
-pub fn check() -> bool {
-    let dirpath = rupmdir();
-    dirpath.exists()
-}
-pub fn test() -> io::Result<()> {
-    let resp = reqwest::blocking::get("https://jmlisowski.github.io/rupm/hello.tar").expect("request failed");
-    let body = resp.text().expect("body invalid");
-    let mut out = File::create("hello.tar").expect("failed to create file");
-    io::copy(&mut body.as_bytes(), &mut out).expect("failed to copy content");
-    Ok(())
-}
-pub fn install(pkg: &str) -> Result<(), io::Error>{
-    let link = format!("https://jmlisowski.github.io/rupm/{}.tar", pkg);
-    let filename = format!("{}.tar", pkg);
-    let link = link.as_str();
-    let filename = filename.as_str();
-    download(&link, &filename);
-
+fn extract(filename: &str) -> io::Result<()> {
     let path = filename;
     let tar = File::open(path)?;
     let mut archive = Archive::new(tar);
     archive.unpack(rupmdir())
 }
-pub fn init() -> io::Result<()> {
-    let dirpath = rupmdir();    
-    println!("creating rupm directory at: {:?}", dirpath);
-    create_dir(dirpath)
+pub fn check() -> bool {
+    let dirpath = rupmdir();
+    dirpath.exists()
+}
+pub fn test() {
+    let resp = reqwest::blocking::get("https://jmlisowski.github.io/rupm-packages/hello.tar").expect("request failed");
+    let body = resp.text().expect("body invalid");
+    let mut out = File::create("hello.tar").expect("failed to create file");
+    io::copy(&mut body.as_bytes(), &mut out).expect("failed to copy content");
+}
+pub fn install(pkg: &str) {
+    let link = format!("https://jmlisowski.github.io/rupm-packages/{}.tar", pkg);
+    let filename = format!("{}.tar", pkg);
+    let link = link.as_str();
+    let filename = filename.as_str();
+    download(&link, &filename);
+    extract(&filename).expect("failed to extract tarball");
+    remove_file(&filename).expect("failed to remove file");
 }
 
-pub fn help() -> io::Result<()> {
+pub fn init() {
+    let dirpath = rupmdir();
+    let binpath: PathBuf = (dirpath.to_string_lossy().to_string() + "/bin").into();
+    println!("creating rupm directory at: {:?}", dirpath);
+
+    set_env::append("PATH", &binpath.to_str().unwrap()).expect("Couldn't find PATH");
+    create_dir_all(&binpath).expect("failed to make directory");
+}
+
+pub fn help() {
     println!("Commands for rupm: 
     init | sets up rupm for use
     help | displays help
     install [package name] | installs a package
     remove [package name] | removes a package");
-    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
 use colored::Colorize;
-use std::{
-    env,
-    io,
-};
+use std::env;
 
-fn main() -> io::Result<()> {
+fn main() {
     let args: Vec<String> = env::args().collect();
     if !lib::check() && &args[1] != "init" {
         println!("{}", "rupm not set up yet! run comand 'rupm init' to use rupm.".red().bold());
     }
     if args.len() > 1 {
         if &args[1] == "help" {
-            lib::help()
+            lib::help();
         } else if &args[1] == "install" {
-            lib::install(&args[2])
+            lib::install(&args[2]);
         } else if &args[1] == "test" {
-            lib::test()
+            lib::test();
         } else if &args[1] == "init" {
-            lib::init()
+            lib::init();
         } else {
             println!("{} '{}'", "couldn't recognize command".red().bold(), &args[1].red().bold());
-            lib::help()
+            lib::help();
         }
     } else {
-        lib::help()
+        lib::help();
     }
 }
