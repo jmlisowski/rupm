d5a63dd24f39521053f4a5cc5d32053fe86e7c68
jmlisowski <jake.lisowski5@gmail.com>
2023-02-13 20:12:06 +0000

    make changes... yet again.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -13,11 +13,13 @@ fn rupmdir() -> PathBuf {
     let dirpath: std::path::PathBuf = (homedir.to_string_lossy().to_string() + "/.rupm").into();
     dirpath
 }
-
 fn download(link: &str, filename: &str) {
     let mut resp = reqwest::blocking::get(link).expect("request failed");
     let mut out = File::create(filename).expect("failed to create file");
     io::copy(&mut resp, &mut out).expect("failed to copy content");
+}
+fn update() {
+    download("https://jmlisowksi.github.io/rupm-packages/packages", "packages");
 }
 fn extract(filename: &str) -> io::Result<()> {
     let path = filename;
@@ -42,16 +44,15 @@ pub fn install(pkg: &str) {
     println!("cleaning up");
     remove_file(&filename).expect("failed to remove file");
 }
-
 pub fn init() {
     let dirpath = rupmdir();
     let binpath: PathBuf = (dirpath.to_string_lossy().to_string() + "/bin").into();
     println!("creating rupm directory at: {:?}", dirpath);
+    println!("adding {:?} to PATH", binpath);
 
     set_env::append("PATH", &binpath.to_str().unwrap()).expect("Couldn't find PATH");
     create_dir_all(&binpath).expect("failed to make directory");
 }
-
 pub fn help() {
     println!("Commands for rupm: 
     init | sets up rupm for use
