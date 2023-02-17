d1ac476f2b9ea24fdd78efc5fec24d3ef7f43283
jmlisowski <jake.lisowski5@gmail.com>
2023-02-16 23:44:32 -0600

    improved a lot of shit

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -2,12 +2,41 @@ extern crate reqwest;
 use dirs;
 use colored::Colorize;
 use tar::Archive;
-use std::path::PathBuf;
-use std::io::{self, Read};
-use std::fs::{
-    File,
-    remove_file,
+use serde::{Serialize, Deserialize};
+use ron::de::from_reader;
+use std::{
+    path::PathBuf,
+    io::{
+        self,
+        BufReader,
+    },
+    fs::{
+        File,
+        remove_file,
+    },
 };
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Package {
+    package: String,
+    version: String,
+}
+
+fn get_package(name: &str) -> Option<Package> {
+    let filepath: PathBuf = (rupmdir().to_string_lossy().to_string() + "/packages.ron").into();
+    let filepath = filepath.to_str().unwrap();
+    let file = File::open(filepath).expect("unable to read file");
+    let reader = BufReader::new(file);
+    let packages: Vec<Package> = from_reader(reader).expect("cannot deserialize");
+    //println!("{:?}", packages);
+    for package in packages {
+        if package.package == name {
+            return Some(package);
+        }
+    }
+
+    None
+}
 
 fn rupmdir() -> PathBuf {
     let homedir = dirs::home_dir().unwrap();
@@ -20,11 +49,11 @@ fn download(link: &str, filename: &str) {
     io::copy(&mut resp, &mut out).expect("failed to copy content");
 }
 pub fn update() {
-    let filepath: PathBuf = (rupmdir().to_string_lossy().to_string() + "/packages.txt").into();
+    let filepath: PathBuf = (rupmdir().to_string_lossy().to_string() + "/packages.ron").into();
     let filepath = filepath.to_str().unwrap();
     File::create(&filepath).expect("failed to create file");
     println!("updating package list");
-    download("https://raw.githubusercontent.com/jmlisowski/rupm-packages/main/packages.txt", filepath);
+    download("https://raw.githubusercontent.com/jmlisowski/rupm-packages/main/packages.ron", filepath);
 }
 fn extract(filename: &str) -> io::Result<()> {
     let path = filename;
@@ -33,42 +62,46 @@ fn extract(filename: &str) -> io::Result<()> {
     archive.unpack(rupmdir())
 }
 pub fn install(pkg: &str) {
-    let link = format!("https://raw.githubusercontent.com/jmlisowski/rupm-packages/main/{}.tar", pkg);
-    let link = link.as_str();
-    let filename = format!("{}.tar", pkg);
-    let filename = filename.as_str();
-    let filepath: PathBuf = (rupmdir().to_string_lossy().to_string() + "/packages.txt").into();
-    let filepath = filepath.to_str().unwrap();
     update();
-    let mut file = File::open(filepath).unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("cannot read file");
-    if contents.contains(&pkg) {
-        println!("downloading {}.tar", &pkg);
-        download(link, &filename);
-        println!("extracting {}.tar", &pkg);
-        extract(filename).expect("failed to extract tarball");
-        println!("cleaning up");
-        remove_file(&filename).expect("failed to remove file");
-    } else {
-        println!("{}",format!("{} is not a package!",&pkg).red().bold());
+    let package = get_package(pkg);
+    match package {
+        Some(pkg) => {
+            let link = format!("https://raw.githubusercontent.com/jmlisowski/rupm-packages/main/{}.tar", pkg.package);
+            let link = link.as_str();
+            let filename = format!("{}.tar", pkg.package);
+            let filename = filename.as_str();
+            println!("downloading {}.tar", pkg.package);
+            download(link, filename);
+            println!("extracting {}.tar", pkg.package);
+            extract(filename).expect("failed to extract tarball");
+            println!("cleaning up");
+            remove_file(filename).expect("failed to remove file");
+        }
+        None => {
+            println!("{}",format!("{} is not a package!",pkg).red().bold());
+        }
     }
 }
 pub fn remove(pkg: &str) {
-    let binpath: PathBuf = (rupmdir().to_string_lossy().to_string() + "/bin/").into();
-    let pkgpath: PathBuf = (binpath.to_string_lossy().to_string() + pkg).into();
-    let pkgpath = pkgpath.to_str().unwrap();
-    let filepath: PathBuf = (rupmdir().to_string_lossy().to_string() + "/packages.txt").into();
-    let filepath = filepath.to_str().unwrap();
     update();
-    let mut file = File::open(filepath).unwrap();
-    let mut contents = String::new();
-    //if contents.contains(&pkg)
-    println!("{}",pkgpath);
+    let package = get_package(pkg);
+    match package {
+        Some(pkg) => {
+            let binpath: PathBuf = (rupmdir().to_string_lossy().to_string() + "/bin/").into();
+            let pkgpath: PathBuf = (binpath.to_string_lossy().to_string() + pkg.package.as_str()).into();
+            let pkgpath = pkgpath.to_str().unwrap();
+            println!("removing {}", pkg.package);
+            remove_file(&pkgpath).expect("cannot remove file");
+        }
+        None => {
+            println!("{}",format!("{} is not a package!",pkg).red().bold());
+        }
+    }
 }
 pub fn help() {
     println!("Commands for rupm:
     help | displays help
+    update | updates package list
     install [package name] | installs a package
     remove [package name] | removes a package");
 }
--- rupm_installer/lib/src/lib.rs
+++ rupm_installer/lib/src/lib.rs
@@ -2,12 +2,42 @@ extern crate reqwest;
 use dirs;
 use colored::Colorize;
 use tar::Archive;
-use std::path::PathBuf;
-use std::io::{self, Read};
-use std::fs::{
-    File,
-    create_dir_all, remove_file,
+use serde::{Serialize, Deserialize};
+use ron::de::from_reader;
+use std::{
+    path::PathBuf,
+    io::{
+        self,
+        BufReader,
+    },
+    fs::{
+        File,
+        create_dir_all,
+        remove_file,
+    },
 };
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Package {
+    package: String,
+    version: String,
+}
+
+fn get_package(name: &str) -> Option<Package> {
+    let filepath: PathBuf = (rupmdir().to_string_lossy().to_string() + "/packages.ron").into();
+    let filepath = filepath.to_str().unwrap();
+    let file = File::open(filepath).expect("unable to read file");
+    let reader = BufReader::new(file);
+    let packages: Vec<Package> = from_reader(reader).expect("cannot deserialize");
+    for package in packages {
+        if package.package == name {
+            return Some(package);
+        }
+    }
+
+    None
+}
+
 
 fn rupmdir() -> PathBuf {
     let homedir = dirs::home_dir().unwrap();
@@ -20,12 +50,11 @@ fn download(link: &str, filename: &str) {
     io::copy(&mut resp, &mut out).expect("failed to copy content");
 }
 fn update() {
-    let filepath: PathBuf = (rupmdir().to_string_lossy().to_string() + "/packages.txt").into();
-    let filepath = filepath.to_string_lossy().to_string();
-    let filepath = filepath.as_str();
+    let filepath: PathBuf = (rupmdir().to_string_lossy().to_string() + "/packages.ron").into();
+    let filepath = filepath.to_str().unwrap();
     File::create(&filepath).expect("failed to create file");
     println!("updating package list");
-    download("https://raw.githubusercontent.com/jmlisowski/rupm-packages/main/packages.txt", filepath);
+    download("https://raw.githubusercontent.com/jmlisowski/rupm-packages/main/packages.ron", filepath);
 }
 fn extract(filename: &str) -> io::Result<()> {
     let path = filename;
@@ -34,26 +63,24 @@ fn extract(filename: &str) -> io::Result<()> {
     archive.unpack(rupmdir())
 }
 fn install(pkg: &str) {
-    let link = format!("https://raw.githubusercontent.com/jmlisowski/rupm-packages/main/{}.tar", pkg);
-    let filename = format!("{}.tar", pkg);
-    let link = link.as_str();
-    let filename = filename.as_str();
-    let filepath: PathBuf = (rupmdir().to_string_lossy().to_string() + "/packages.txt").into();
-    let filepath = filepath.to_string_lossy().to_string();
-    let filepath = filepath.as_str();
     update();
-    let mut file = File::open(filepath).unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("cannot read file");
-    if contents.contains(&pkg) {
-        println!("downloading {}.tar", &pkg);
-        download(link, &filename);
-        println!("extracting {}.tar", &pkg);
-        extract(filename).expect("failed to extract tarball");
-        println!("cleaning up");
-        remove_file(&filename).expect("failed to remove file");
-    } else {
-        println!("{}",format!("{} is not a package!",&pkg).red().bold());
+    let package = get_package(pkg);
+    match package {
+        Some(pkg) => {
+            let link = format!("https://raw.githubusercontent.com/jmlisowski/rupm-packages/main/{}.tar", pkg.package);
+            let link = link.as_str();
+            let filename = format!("{}.tar", pkg.package);
+            let filename = filename.as_str();
+            println!("downloading {}.tar", pkg.package);
+            download(link, filename);
+            println!("extracting {}.tar", pkg.package);
+            extract(filename).expect("failed to extract tarball");
+            println!("cleaning up");
+            remove_file(filename).expect("failed to remove file");
+        }
+        None => {
+            println!("{}",format!("{} is not a package!",pkg).red().bold());
+        }
     }
 }
 pub fn init() {
